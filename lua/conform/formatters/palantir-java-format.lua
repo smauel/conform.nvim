1d3da0db34788ff4384d5ba3051ffbcc0d528e14
Sam Morrison <samuelmorrison87@gmail.com>
2023-11-03 16:06:36 +0000

    feat: add 'palantir-java-format' formatter

---
+++ lua/conform/formatters/palantir-java-format.lua
@@ -0,0 +1,9 @@
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://github.com/palantir/palantir-java-format",
+    description = "Reformats Java source code according to Palantir Java Style.",
+  },
+  command = "palantir-java-format",
+  args = { "-" },
+}
